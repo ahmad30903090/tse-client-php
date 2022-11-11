aa4f1eda75a179050a744a632c6f32fa246d7209
ahmad30903090 <62177717+ahmad30903090@users.noreply.github.com>
2022-11-11 13:41:26 +0330

    Create tse_functions.php

---
+++ tse_functions.php
@@ -0,0 +1,67 @@
+<?php
+/* Najafabad HoshmanRabin Stock Broker(Modaber Asia)
+ * programin by:ahmad mostafaie (ahma3090@yahoo.com)
+ * under GNU/GPL Licence
+ */
+function get_web_id($nemad = "شپنا") {
+    $address = "http://www.tsetmc.com/tsev2/data/search.aspx?skey=" . $nemad;
+
+    $useragent = "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36";
+    $ch = curl_init($address);
+
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_ENCODING, 'UTF-8');
+
+    $response = curl_exec($ch);
+    curl_close($ch);
+
+    $lines = explode(';', $response);
+
+    $sahm = "shepna";
+    $$sahm = array();
+    $counter = 0;
+    foreach ($lines as $key => $value) {
+        $myarr = explode(",", $value);
+        return $myarr[2];
+        $stockvalue = array('nemad' => $myarr[0], 'name' => $myarr[1], 'web_id' => $myarr[2]);
+        array_push($$sahm, $stockvalue);
+        print_r($stockvalue);
+        break;
+        $counter++;
+    }
+    echo 'stock array' . $sahm . 'with ' . $counter . ' Rows values Created !';
+}
+
+function get_stock_price_hist($f_nemad) {
+    $web_id=get_web_id($f_nemad);
+    $address = "http://members.tsetmc.com/tsev2/data/InstTradeHistory.aspx?i=".$web_id."&Top=999999&A=0";
+
+    $useragent = "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36";
+    $ch = curl_init($address);
+
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_ENCODING, 'UTF-8');
+
+    $response = curl_exec($ch);
+    curl_close($ch);
+
+
+    $lines = explode(';', $response);
+
+    $sahm = $f_nemad;
+    $$sahm = array();
+    $counter = 0;
+    foreach ($lines as $key => $value) {
+        $myarr = explode("@", $value);
+        if (count($myarr) != 10)
+            break;
+        $stockvalue = array('Date' => $myarr[0], 'High' => $myarr[1], 'Low' => $myarr[2], 'Final' => $myarr[3], 'Close' => $myarr[4], 'Open' => $myarr[5], 'Y-Final=>$myarr[6]', 'Value' => $myarr[7], 'Volume' => $myarr[8], 'No' => $myarr[9]);
+        array_push($$sahm, $stockvalue);
+        $counter++;
+    }
+    echo 'stock array' . $sahm . 'with ' . $counter . ' Rows values Created !';
+    return $$sahm;
+}
+
+
+ 
